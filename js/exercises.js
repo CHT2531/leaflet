59f15e859e81e7eef6cb8422a01ad8566e51aad9
Matthew Mantle <memantle@googlemail.com>
2019-12-09 15:33:34 +0000

    initial commit

--- js/exercises.js
+++ js/exercises.js
@@ -36,9 +36,9 @@ const takeaways=[
 ];
 
 
-const infoDiv = document.querySelector("#info");
-const nameDiv = document.querySelector("#takeawayName");
-const descDiv = document.querySelector("#takeawayDesc");
+let infoDiv;
+let nameDiv;
+let descDiv;
 
 
 function initMap(){
@@ -48,11 +48,11 @@ function initMap(){
 	 	attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
 	    maxZoom: 18
 	 }).addTo(myMap);
-	 
+
 }
 
 
-function itWorks(position) 
+function itWorks(position)
 {
 	let latitude = position.coords.latitude;
 	let longitude = position.coords.longitude;
@@ -60,7 +60,7 @@ function itWorks(position)
 	console.log('longitude: '+longitude);
 }
 
-function itDoesntWork(error) 
+function itDoesntWork(error)
 {
 	console.log('There is an error '+error);
 }
@@ -70,17 +70,27 @@ function getUserPosition()
     navigator.geolocation.getCurrentPosition(itWorks, itDoesntWork);
 }
 
+function init()
+{
+	infoDiv = document.querySelector("#info");
+	nameDiv = document.querySelector("#takeawayName");
+	descDiv = document.querySelector("#takeawayDesc");
+	getUserPosition()
+}
 
-window.addEventListener("load",getUserPosition,false);
-	
-//1. Open the HTML page in a browser, make sure the console.log displays the latitude and longitude of your current location.
-//2. Add some code that will call the function initMap. This needs to be called after the browser has determined the user's current location. Have a look at https://leafletjs.com/, see if you can make sense of the code in initMap().
+
+init()
+
+//1. Open the HTML page in a browser, make sure the console.log displays the latitude and longitude of your current location. Have a look at your code and try and work out which functions are being called and in what order.
+//2.
+//a) Add some code that will call the function initMap. This needs to be called after the browser has determined the user's current location. If you can get this to work a map should appear. Have a look at https://leafletjs.com/, see if you can make sense of the code in initMap().
+//b) Sometimes I've had problems with the openstreetmap provider. See https://leaflet-extras.github.io/leaflet-providers/preview/ for alternative providers
 //3. Modify initMap so that the map is centred on the user's current location. You will need to pass the latitude and longitude as arguments to the initMap() function.
-//4. Add a marker to the map (see https://leafletjs.com/reference-1.3.4.html#marker) at the user's location.
+//4. Add a marker to the map (see https://leafletjs.com/reference-1.6.0.html#marker) at the user's location.
 //5. Add a popup to the marker that says 'You are here'. The popup should appear when the page loads.
-//6. The variable takeaways (at the top of the script) is an array of GeoJSON objects. Add a forEach in the initMap() function that loops over this array and displays the name of each takeaway in the console. 
+//6. The variable takeaways (at the top of the script) is an array of objects. Add a forEach in the initMap() function that loops over this array and displays the name of each takeaway in the console.
 //7. Modify this so that the loop also displays the latitude of each takeaway
 //8. Modify this so that the loop  adds markers to the map showing the location of each takeaway. You might have to adjust the zoom level so you can see all the markers
-//9. Modify the loop so that you make each marker clickable. When clicked the name of the takeaway should be displayed in the console. It would probably be a good idea to use a closure to do this. Have a look at the example at the end of last week's lecture slides. 
-//10. Instead of displaying the details for the takeaway in the console.log can you display them in the HTML page, in the div called 'info'. It already has some CSS applied so that it overlaps the map. 
-//11. Have a look at https://leafletjs.com/reference-1.3.4.html#latlngbounds. How can you make the map automatically adjust the zoom level to show all the markers?
+//9. Modify the loop so that you make each marker clickable. When clicked the name of the takeaway should be displayed in the console. It would probably be a good idea to use a closure to do this. Have a look at the example from last week or https://github.com/CHT2531/functional-programming/blob/master/notes.md#closures---associating-data-with-dynamically-generated-elements.
+//10. Instead of displaying the details for the takeaway in the console.log can you display them in the HTML page, in the div called 'info'. It already has some CSS applied so that it overlaps the map.
+//11. Have a look at https://leafletjs.com/reference-1.6.0.html#latlngbounds. How can you make the map automatically adjust the zoom level to show all the markers?
